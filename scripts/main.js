d91c64f4a43d3822b5584f91bb10e4f070e33f6e
Yannick Lemin <yle@techyos.com>
2025-05-13 22:26:11 +0200

    added audio resume as well, because why not?

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+$(document).ready(function() {
+  // Initially hide the podcast section
+  $('#podcast').hide();
+
+  // Toggle podcast section visibility when podcast icon is clicked
+  $('#podcast-toggle').click(function(e) {
+    e.preventDefault();
+    $('#podcast').slideToggle(300);
+  });
+});
